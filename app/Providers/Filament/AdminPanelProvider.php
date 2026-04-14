587a97b4779a1c8eb8430d3d37bd91317052c5b1
FaatPrasojo <faatprasojo@gmail.com>
2026-04-14 14:35:33 +0700

    login 2 role

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,6 +27,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
+            ->authGuard('customer')
             ->login()
             ->colors([
                 'primary' => Color::Amber,
